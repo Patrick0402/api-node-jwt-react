63006a53b69d7ed67866b1dc2f407ec9df42749f
Patrick0402 <bruno.patrick0402@gmail.com>
2024-09-17 21:49:30 -0300

    Validação no backend

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -11,23 +11,29 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 //Cadastro
 router.post("/cadastro", async (req, res) => {
-  try {
-    const user = req.body;
+  if (req.body.email && req.body.name && req.body.password) {
+    try {
+      const user = req.body;
 
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(user.password, salt);
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(user.password, salt);
 
-    const userDB = await prisma.user.create({
-      data: {
-        email: user.email,
-        name: user.name,
-        password: hashPassword,
-      },
-    });
+      const userDB = await prisma.user.create({
+        data: {
+          email: user.email,
+          name: user.name,
+          password: hashPassword,
+        },
+      });
 
-    res.status(201).json(userDB);
-  } catch (err) {
-    res.status(500).json({ message: "Erro no servidor, tente novamente!" });
+      res.status(201).json(userDB);
+    } catch (err) {
+      res.status(500).json({ message: "Erro no servidor, tente novamente!" });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ message: "Preencha todos os campos adequadamente!" });
   }
 });
 
